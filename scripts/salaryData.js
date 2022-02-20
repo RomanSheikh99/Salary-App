83f36d3c025026e7a6cbd8502e1d74d203b0bc6e
roman <romansheikh170@gmail.com>
2022-02-20 23:54:10 +0600

    first commit

---
+++ scripts/salaryData.js
@@ -0,0 +1,18 @@
+let salary_data = {
+  0: {
+    name: "Addy Osmani",
+    salary: "76340.0"
+  },
+  1: {
+    name: "Paul Irish",
+    salary: "94320.0"
+  },
+  2: {
+    name: "Pete Hunt",
+    salary: "67000.0"
+  },
+  3: {
+    name: "Pete sampras",
+    salary: "105540.0"
+  }
+};
